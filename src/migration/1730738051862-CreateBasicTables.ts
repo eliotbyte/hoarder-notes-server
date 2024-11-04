44a165a40e222f0b0e916bad522e3de04e35c161
eliotbyte <eliotbyte+github@proton.me>
2024-11-04 19:37:21 +0300

    rename table user to users

--- src/migration/1730738051862-CreateBasicTables.ts
+++ src/migration/1730738051862-CreateBasicTables.ts
@@ -1,13 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddNotesTagsAndNoteTagsTables1730661548835
-  implements MigrationInterface
-{
-  name = 'AddNotesTagsAndNoteTagsTables1730661548835';
+export class CreateBasicTables1730738051862 implements MigrationInterface {
+  name = 'CreateBasicTables1730738051862';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "notes" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "modified_at" TIMESTAMP NOT NULL DEFAULT now(), "text" text NOT NULL, "parent_id" integer, "user_id" integer NOT NULL, "is_deleted" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_af6206538ea96c4e77e9f400c3d" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "users" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "modified_at" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying(100) NOT NULL, "password_hash" character varying(255) NOT NULL, CONSTRAINT "UQ_51b8b26ac168fbe7d6f5653e6cf" UNIQUE ("name"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE TABLE "tags" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "modified_at" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying(50) NOT NULL, CONSTRAINT "PK_e7dc17249a1148a1970748eda99" PRIMARY KEY ("id"))`,
@@ -16,7 +14,7 @@ export class AddNotesTagsAndNoteTagsTables1730661548835
       `CREATE TABLE "note_tags" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "modified_at" TIMESTAMP NOT NULL DEFAULT now(), "note_id" integer NOT NULL, "tag_id" integer NOT NULL, CONSTRAINT "PK_ca61a805f00b069d6a9ec15b56b" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `ALTER TABLE "notes" ADD CONSTRAINT "FK_7708dcb62ff332f0eaf9f0743a7" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+      `CREATE TABLE "notes" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "modified_at" TIMESTAMP NOT NULL DEFAULT now(), "text" text NOT NULL, "parent_id" integer, "user_id" integer NOT NULL, "is_deleted" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_af6206538ea96c4e77e9f400c3d" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "note_tags" ADD CONSTRAINT "FK_6fa35b8ead30ef28cc1ac377b21" FOREIGN KEY ("note_id") REFERENCES "notes"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
@@ -24,20 +22,24 @@ export class AddNotesTagsAndNoteTagsTables1730661548835
     await queryRunner.query(
       `ALTER TABLE "note_tags" ADD CONSTRAINT "FK_898115de9eadba996d4323ff0b6" FOREIGN KEY ("tag_id") REFERENCES "tags"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
+    await queryRunner.query(
+      `ALTER TABLE "notes" ADD CONSTRAINT "FK_7708dcb62ff332f0eaf9f0743a7" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "notes" DROP CONSTRAINT "FK_7708dcb62ff332f0eaf9f0743a7"`,
+    );
     await queryRunner.query(
       `ALTER TABLE "note_tags" DROP CONSTRAINT "FK_898115de9eadba996d4323ff0b6"`,
     );
     await queryRunner.query(
       `ALTER TABLE "note_tags" DROP CONSTRAINT "FK_6fa35b8ead30ef28cc1ac377b21"`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "notes" DROP CONSTRAINT "FK_7708dcb62ff332f0eaf9f0743a7"`,
-    );
+    await queryRunner.query(`DROP TABLE "notes"`);
     await queryRunner.query(`DROP TABLE "note_tags"`);
     await queryRunner.query(`DROP TABLE "tags"`);
-    await queryRunner.query(`DROP TABLE "notes"`);
+    await queryRunner.query(`DROP TABLE "users"`);
   }
 }
