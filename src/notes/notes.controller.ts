ce5fb11251faaa7e929969723ccb764ffe310724
eliotbyte <eliotbyte+github@proton.me>
2024-11-09 21:49:28 +0300

    fix spaceId is not required if parentId is specified

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -62,9 +62,9 @@ export class NotesController {
   async getAllNotes(@Req() req, @Query() query: any) {
     const userId = req.user.userId;
 
-    // Ensure spaceId is provided
-    if (!query.spaceId) {
-      throw new BadRequestException('spaceId is a required parameter');
+    // Ensure spaceId is provided if parentId is not provided
+    if (!query.spaceId && !query.parentId) {
+      throw new BadRequestException('Either spaceId or parentId is required');
     }
 
     const filters = {
@@ -72,7 +72,7 @@ export class NotesController {
       parentId: query.parentId ? Number(query.parentId) : undefined,
       notReply: query.notReply === 'true',
       tags: query.tags ? query.tags.split(',') : [],
-      spaceId: Number(query.spaceId),
+      spaceId: query.spaceId ? Number(query.spaceId) : undefined,
       topicId: query.topicId ? Number(query.topicId) : undefined,
       page: query.page ? Number(query.page) : 1,
       pageSize: query.pageSize ? Number(query.pageSize) : 10,
