743fd016fbeaaaa2606ae65ce0fca62a0619b247
eliotbyte <eliotbyte+github@proton.me>
2025-01-08 02:51:30 +0300

    fix deploy secrets

--- data-source.ts
+++ data-source.ts
@@ -3,11 +3,11 @@ import { join } from 'path';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5433,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'hoarder_notes_db',
+  host: process.env.DB_HOST || 'db',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'hoarder_notes_db',
   entities: [join(__dirname, '**', '*.entity.{ts,js}')],
   migrations: ['src/migration/*.ts'],
   synchronize: false,
